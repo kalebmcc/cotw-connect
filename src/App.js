7b997bc270dcc6312301548fcfa3656e88d950a0
kalebmcc <kalebl33mccullough@gmail.com>
2023-07-06 00:47:00 -0500

    testing bootstrap functionality - still not working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Calendar from './components/calendar';
 import Alert from './components/alert';
+import ThemeSwitcher from './components/ThemeSwitcher';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<h1>COTW Connect</h1>
-				<Calendar/>
-        <Alert/>
+				<Calendar />
+				<Alert />
+				<ThemeSwitcher />
 			</header>
 		</div>
 	);
--- src/components/alert.js
+++ src/components/alert.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
+import * as bootstrap from 'bootstrap';
 
 class Alert extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {  }
-    render() { 
-        return ( 
+	constructor(props) {
+		super(props);
+	}
+	state = {};
+	render() {
+		window.Modal = bootstrap.Toast;
 
-            <h2>new thing happened</h2>
+		const toastTrigger = document.getElementById('liveToastBtn');
+		const toastLiveExample = document.getElementById('liveToast');
 
-         );
-    }
+		if (toastTrigger) {
+			const toastBootstrap =
+				bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+			toastTrigger.addEventListener('click', () => {
+				toastBootstrap.show();
+			});
+		}
+		return (
+			<div>
+				<button type='button' class='btn btn-primary' id='liveToastBtn'>
+					Show live toast
+				</button>
+
+				<div class='toast-container position-fixed bottom-0 end-0 p-3'>
+					<div
+						id='liveToast'
+						class='toast'
+						role='alert'
+						aria-live='assertive'
+						aria-atomic='true'>
+						<div class='toast-header'>
+							<img src='...' class='rounded me-2' alt='...' />
+							<strong class='me-auto'>Bootstrap</strong>
+							<small>11 mins ago</small>
+							<button
+								type='button'
+								class='btn-close'
+								data-bs-dismiss='toast'
+								aria-label='Close'></button>
+						</div>
+						<div class='toast-body'>Hello, world! This is a toast message.</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
- 
+
 export default Alert;
